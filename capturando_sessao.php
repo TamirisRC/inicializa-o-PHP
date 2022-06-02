3ab04556bc7c5c00381755a4af18ab740ac87735
TamirisRC <tamiris.carvalho5@etec.sp.gov.br>
2022-06-02 15:59:00 -0300

    Inicialização PHP

---
+++ capturando_sessao.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+/*session is started if you don´t write this line can´t use $_Session global variable */
+$_SESSION["newsession"]=$value;
+/*session created*/ 
+echo $_SESSION["newsession"];
+/*session was getting*/
+?> 
